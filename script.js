4a9d11c72545ef476c33804422fea7509c58e6c6
onom <onomuta@gmail.com>
2018-05-24 12:59:07 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 navigator.requestMIDIAccess().then(onMIDISuccess,onMIDIFailure);
 var midi = null;
 var inputs = [];
@@ -85,30 +86,32 @@ $(function() {
     }
   );
 });
-$("#knob1").knob({
-  'change' : function (v) {sendCC(16, Math.round(v))},
-});
-$("#knob2").knob({
-  'change' : function (v) {sendCC(17, Math.round(v))},
-});
-$("#knob3").knob({
-  'change' : function (v) {sendCC(18, Math.round(v))},
-});
-$("#knob4").knob({
-  'change' : function (v) {sendCC(19, Math.round(v))},
-});
-$("#knob5").knob({
-  'change' : function (v) {sendCC(20, Math.round(v))},
-});
-$("#knob6").knob({
-  'change' : function (v) {sendCC(21, Math.round(v))},
-});
-$("#knob7").knob({
-  'change' : function (v) {sendCC(22, Math.round(v))},
-});
-$("#knob8").knob({
-  'change' : function (v) {sendCC(23, Math.round(v))},
-});
+$("#knob1-1").knob({ 'change' : function (v) {sendCC(13, Math.round(v))}, });
+$("#knob1-2").knob({ 'change' : function (v) {sendCC(14, Math.round(v))}, });
+$("#knob1-3").knob({ 'change' : function (v) {sendCC(15, Math.round(v))}, });
+$("#knob1-4").knob({ 'change' : function (v) {sendCC(16, Math.round(v))}, });
+$("#knob1-5").knob({ 'change' : function (v) {sendCC(17, Math.round(v))}, });
+$("#knob1-6").knob({ 'change' : function (v) {sendCC(18, Math.round(v))}, });
+$("#knob1-7").knob({ 'change' : function (v) {sendCC(19, Math.round(v))}, });
+$("#knob1-8").knob({ 'change' : function (v) {sendCC(20, Math.round(v))}, });
+
+$("#knob2-1").knob({ 'change' : function (v) {sendCC(16, Math.round(v))}, });
+$("#knob2-2").knob({ 'change' : function (v) {sendCC(17, Math.round(v))}, });
+$("#knob2-3").knob({ 'change' : function (v) {sendCC(18, Math.round(v))}, });
+$("#knob2-4").knob({ 'change' : function (v) {sendCC(19, Math.round(v))}, });
+$("#knob2-5").knob({ 'change' : function (v) {sendCC(20, Math.round(v))}, });
+$("#knob2-6").knob({ 'change' : function (v) {sendCC(21, Math.round(v))}, });
+$("#knob2-7").knob({ 'change' : function (v) {sendCC(22, Math.round(v))}, });
+$("#knob2-8").knob({ 'change' : function (v) {sendCC(23, Math.round(v))}, });
+
+$("#knob3-1").knob({ 'change' : function (v) {sendCC(49, Math.round(v))}, });
+$("#knob3-2").knob({ 'change' : function (v) {sendCC(50, Math.round(v))}, });
+$("#knob3-3").knob({ 'change' : function (v) {sendCC(51, Math.round(v))}, });
+$("#knob3-4").knob({ 'change' : function (v) {sendCC(52, Math.round(v))}, });
+$("#knob3-5").knob({ 'change' : function (v) {sendCC(53, Math.round(v))}, });
+$("#knob3-6").knob({ 'change' : function (v) {sendCC(54, Math.round(v))}, });
+$("#knob3-7").knob({ 'change' : function (v) {sendCC(55, Math.round(v))}, });
+$("#knob3-8").knob({ 'change' : function (v) {sendCC(56, Math.round(v))}, });
 
 // SLIDER
 $( ".slider" ).slider({
@@ -176,27 +179,16 @@ $( "#btn5B" ).mousedown(function() { pushBtn(52,this) });
 $( "#btn6B" ).mousedown(function() { pushBtn(53,this) });
 $( "#btn7B" ).mousedown(function() { pushBtn(54,this) });
 $( "#btn8B" ).mousedown(function() { pushBtn(55,this) });
-$( "#btn1C" ).mousedown(function() { pushBtn(64,this) });
-$( "#btn2C" ).mousedown(function() { pushBtn(65,this) });
-$( "#btn3C" ).mousedown(function() { pushBtn(66,this) });
-$( "#btn4C" ).mousedown(function() { pushBtn(67,this) });
-$( "#btn5C" ).mousedown(function() { pushBtn(68,this) });
-$( "#btn6C" ).mousedown(function() { pushBtn(69,this) });
-$( "#btn7C" ).mousedown(function() { pushBtn(70,this) });
-$( "#btn8C" ).mousedown(function() { pushBtn(71,this) });
 
 
-$( "#func1" ).mousedown(function() { pushBtn(58,this) });
-$( "#func2" ).mousedown(function() { pushBtn(59,this) });
-$( "#func3" ).mousedown(function() { pushBtn(46,this) });
-$( "#func4" ).mousedown(function() { pushBtn(60,this) });
-$( "#func5" ).mousedown(function() { pushBtn(61,this) });
-$( "#func6" ).mousedown(function() { pushBtn(62,this) });
-$( "#func7" ).mousedown(function() { pushBtn(43,this) });
-$( "#func8" ).mousedown(function() { pushBtn(44,this) });
-$( "#func9" ).mousedown(function() { pushBtn(42,this) });
-$( "#func10").mousedown(function() { pushBtn(41,this) });
-$( "#func11").mousedown(function() { pushBtn(45,this) });
+$( "#funcN" ).mousedown(function() { pushBtn(58,this) });
+$( "#funcS" ).mousedown(function() { pushBtn(59,this) });
+$( "#funcW" ).mousedown(function() { pushBtn(46,this) });
+$( "#funcE" ).mousedown(function() { pushBtn(60,this) });
+$( "#func1" ).mousedown(function() { pushBtn(105,this) });
+$( "#func2" ).mousedown(function() { pushBtn(106,this) });
+$( "#func3" ).mousedown(function() { pushBtn(107,this) });
+$( "#func4" ).mousedown(function() { pushBtn(108,this) });
 
 
 function pushBtn(cc,t){
